2a76701045c435d46d7029b0e776dcabf717d33d
Warley <warleylopes@outlook.com>
2018-07-31 11:47:55 -0300

    Update WebTestAutomationDemo.groovy
    Fast fixes upon being available to compile the past code, refactoring for the jquery selectors is still pending.

--- WebTestAutomationDemo.groovy
+++ WebTestAutomationDemo.groovy
@@ -12,29 +12,35 @@ import geb.spock.GebSpec
 import geb.spock.GebReportingSpec
 
 //After running the script for the first time with GroovyConsole remove the two below lines
-System.setProperty('webdriver.chrome.driver','/webdrivers/win32/chromedriver.exe')
-System.setProperty('geb.build.reportsDir','/reports')
+System.setProperty('webdriver.chrome.driver','../../webdrivers/win32/chromedriver.exe')
+System.setProperty('geb.build.reportsDir','../../reports')
 
 class simpleNavigationSpec extends GebReportingSpec {
-    def setup() {}		// runs before every feature method
-    def cleanup() {}	// runs after every feature method
-    def setupSpec() {}	// runs before the first feature method
+    def setup() {}        // runs before every feature method
+    def cleanup() {}    // runs after every feature method
+    def setupSpec() {}    // runs before the first feature method
     
-    def cleanupSpec() {	// runs after the last feature method
+    def cleanupSpec() {    // runs after the last feature method
          resetBrowser()
          CachingDriverFactory.clearCacheAndQuitDriver()
     }
     
     def "should check if BairesDev Glassdoor rating is above 4.0"(){
-        when: "I look for BairesDev Glassdoor page"
-            go "bairesdev reviews glassdoor"
-			
-		and: "I click on the link for the desired page"
-			waitFor{$("a:contains('BairesDev Reviews | Glassdoor')",0).displayed}
-			$("a:contains('BairesDev Reviews | Glassdoor')",0).click()
+        when: "navigating to Google"
+            go "https://www.google.com/"
+            
+        and: "searching for BairesDev Glassdoor reviews page"
+            waitFor{$("#lst-ib").displayed}
+            $("#lst-ib").value("BairesDev Glassdoor reviews")
+            $("#tsf > div.tsf-p > div.jsb > center > input[type='submit']:nth-child(1)").click()
+            
+        and: "I click on the link for the desired page"
+            waitFor{$("#rso > div > div > div:nth-child(1) > div > div > h3 > a").displayed}
+            $("#rso > div > div > div:nth-child(1) > div > div > h3 > a").click()
         
         then: "The rating should be above 4.0"
-			waitFor{$("div.ratingNum",0).displayed}
-			$("div.ratingNum",0)>4
+            waitFor{$("#EmpStats > div > div.ratingInfo.tighten.cf.fullWidth > div.ratingNum").displayed}
+            float ratingValue = $("#EmpStats > div > div.ratingInfo.tighten.cf.fullWidth > div.ratingNum")[0].text() as Float
+            assert ratingValue>4
     }
 }
